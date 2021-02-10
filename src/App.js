24033849dc4450b8adca8c9bed11de5e0f487378
Rejan Bajracharya <buzz.rejan@gmail.com>
2021-02-10 21:13:24 +0545

    rolldice with styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import logo from "./logo.svg";
+import RollDice from "./RollDice";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RollDice />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Die.js
@@ -0,0 +1,9 @@
+import reactDom from "react-dom";
+import React from "react";
+import "./Die.css";
+
+export default class Die extends React.Component {
+  render() {
+    return <i className={`Die fas fa-dice-${this.props.face} `}></i>;
+  }
+}
